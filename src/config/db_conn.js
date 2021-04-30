042f396368e8bd10c32b145af01587bdcb3e4b01
aratnamirasugab <i.mantara@ralali.com>
2021-04-30 21:39:24 +0700

    updated db config

--- src/config/db_conn.js
+++ src/config/db_conn.js
@@ -3,11 +3,13 @@
 const mysql = require('mysql');
 
 const con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "middleman",
+    host: "us-cdbr-east-03.cleardb.com",
+    user: "be9afa0b8c3d9f",
+    password: "74b0db4e",
+    database: "heroku_227f99b4a734a11",
     multipleStatements : true
 });
 
 module.exports = con;
+
+//mysql://be9afa0b8c3d9f:74b0db4e@us-cdbr-east-03.cleardb.com/heroku_227f99b4a734a11?reconnect=true
